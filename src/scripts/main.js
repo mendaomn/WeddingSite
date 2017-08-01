66fc2192112729178b3c2db931a9af0bbee707f6
Alessandro Menduni <menduni6@gmail.com>
2017-08-01 12:08:52 +0200

    Add countdown

---
+++ src/scripts/main.js
@@ -0,0 +1,25 @@
+// Elementi in cui mettiamo i valori del countdown
+var mesiEl = document.getElementById('cd-mesi');
+var giorniEl = document.getElementById('cd-giorni');
+var oreEl = document.getElementById('cd-ore');
+var minutiEl = document.getElementById('cd-minuti');
+var secondiEl = document.getElementById('cd-secondi');
+
+// Data di arrivo
+var weddingDate = new Date( 2018, 6, 7 );
+
+// Primo giro senza aspettare un secondo
+updateCountdown();
+
+// Avvia un loop ogni secondo
+setInterval( updateCountdown, 1000);
+
+// Calcola e mostra a schermo
+function updateCountdown() {
+  var result = countdown(weddingDate);
+  mesiEl.innerHTML = result.months;
+  giorniEl.innerHTML = result.days;
+  oreEl.innerHTML = result.hours;
+  minutiEl.innerHTML = result.minutes;
+  secondiEl.innerHTML = result.seconds;
+}
