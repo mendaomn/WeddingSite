c6481ee89d08ecef312c87c9006c082149075c45
Alessandro Menduni <menduni@westwingsolutions.com>
2018-02-05 13:02:17 +0100

    Add real photos and minor changes to layout

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,25 +1,62 @@
-// Elementi in cui mettiamo i valori del countdown
-var mesiEl = document.getElementById('cd-mesi');
-var giorniEl = document.getElementById('cd-giorni');
-var oreEl = document.getElementById('cd-ore');
-var minutiEl = document.getElementById('cd-minuti');
-var secondiEl = document.getElementById('cd-secondi');
+function main () {
+  initCountdown()
+  initVideoLightbox()
+}
 
-// Data di arrivo
-var weddingDate = new Date( 2018, 6, 7 ); // i mesi partono da 0
+function initVideoLightbox () {
+  var $playBtn = document.getElementById('mh-play-btn')
+  var $overlay = document.getElementById('vlb-overlay')
 
-// Primo giro senza aspettare un secondo
-updateCountdown();
+  $playBtn.addEventListener('click', showLightbox)
+  $overlay.addEventListener('click', hideLightbox)
+}
 
-// Avvia un loop ogni secondo
-setInterval( updateCountdown, 1000);
+function showLightbox () {
+  var $lightbox = document.getElementById('vlb-container')
+  $lightbox.classList.remove('hidden')
+}
 
-// Calcola e mostra a schermo
-function updateCountdown() {
-  var result = countdown(weddingDate);
-  mesiEl.innerHTML = result.months;
-  giorniEl.innerHTML = result.days;
-  oreEl.innerHTML = result.hours;
-  minutiEl.innerHTML = result.minutes;
-  secondiEl.innerHTML = result.seconds;
+function hideLightbox () {
+  var $lightbox = document.getElementById('vlb-container')
+  $lightbox.classList.add('hidden')
+  resetIframeSrc()
+}
+
+function resetIframeSrc () {
+  var $iframe = document.getElementById('vlb-iframe')
+  $iframe.src = $iframe.src
+}
+
+function initCountdown () {
+  // Elementi in cui mettiamo i valori del countdown
+  var mesiEl = document.getElementById('cd-mesi')
+  var giorniEl = document.getElementById('cd-giorni')
+  var oreEl = document.getElementById('cd-ore')
+  var minutiEl = document.getElementById('cd-minuti')
+  var secondiEl = document.getElementById('cd-secondi')
+
+  // Data di arrivo
+  var weddingDate = new Date(2018, 6, 7, 11, 30) // i mesi partono da 0
+
+  // Primo giro senza aspettare un secondo
+  updateCountdown()
+
+  // Avvia un loop ogni secondo
+  setInterval(updateCountdown, 1000)
+
+  // Calcola e mostra a schermo
+  function updateCountdown () {
+    var result = countdown(weddingDate)
+    mesiEl.innerHTML = result.months
+    giorniEl.innerHTML = result.days
+    oreEl.innerHTML = result.hours
+    minutiEl.innerHTML = result.minutes
+    secondiEl.innerHTML = result.seconds
+  }
+}
+
+if (document.attachEvent ? document.readyState === 'complete' : document.readyState !== 'loading') {
+  main()
+} else {
+  document.addEventListener('DOMContentLoaded', main)
 }
